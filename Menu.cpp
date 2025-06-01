1bfc1b51a55beb1ba3567813c273680071a4ea92
khaitqtuna <khaitqptcnn@gmail.com>
2025-06-01 22:03:44 +0700

    add menu

---
+++ Menu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void menu(){
+    cout<< "1. Gioi thieu ve trung tam" << endl;
+    cout<< "2. Quan ly" << endl;
+    cout<< "3. Thong ke" << endl;
+    cout<< endl << "Lua chon:";
+    int choice = 0;
+    cin>> choice;
+
+    if(choice == 1)
+        //hàm txt giới thiệu 
+    else if(choice == 2)
+        //systemManagement()
+    else if(choice == 3)
+        //ham thong ke txt
+    else
+        do{
+            cout<< endl << "Chu so khong hop le, moi nhap lai:";
+            cin >> choice;
+        }
+        while(choice < 1 || choice > 3);
+}
+
+void systemManagement(QLKH* ql){
+    cout<< "1. Khoa hoc" << endl;
+    cout<< "2. Giao vien" << endl;
+    cout<< "3. Lop hoc" << endl;
+    cout<< "4. Hoc vien" << endl;
+    cout<< endl << "Lua chon:";
+}
+
+void
+
+int main(){
+
+}

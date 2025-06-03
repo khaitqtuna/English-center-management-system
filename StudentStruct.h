afb5714a729c3401589e7f9fb13d224d83f04f9c
lelinh09122005 <linhdepzaixyz09122005@gmail.com>
2025-06-03 16:54:51 +0700

    StudentStruct.h

--- StudentStruct.h
+++ StudentStruct.h
@@ -1,14 +1,22 @@
 #pragma once
-#include "StudentStruct.cpp"
+#include <string>
+using namespace std;
 
+struct Student {
+    string ID;
+    string name;
+    int age;
+    string level;
+    string gender;
+};
+struct NodeStudent {
+    Student data;
+    NodeStudent* next;
+};
 void initStudent(NodeStudent*& S);
-
 NodeStudent* createNodeStudent(Student data);
-
 void addStudent(NodeStudent*& S, Student data);
-
-NodeStudent* searchStudent(NodeStudent* &S, string data);
-
+NodeStudent* searchStudent(NodeStudent*& S, string data);
 Student deleteStudent(NodeStudent*& S, string data);
+void displayStudentInfo(const NodeStudent* S);
 
-void displayStudentInfo(NodeStudent*& S);

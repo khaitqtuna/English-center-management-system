c45971fc959d3fbfa314d8904b3a9fb4145f873a
Vũ Nam Khánh <167059662+kwGuardian@users.noreply.github.com>
2025-06-04 16:49:39 +0700

    Add files via upload

---
+++ KHFile.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<vector>
+
+void exportCoursesToFile(KhoaHoc K, string& KhoaHocFile) {
+    ofstream outfile(KhoaHocFile);
+    if (!outfile) {
+        std::cout << "ERROR: khong the viet vao file!" << endl;
+        return;
+    }
+    QLKH* current = K;
+    while (current != nullptr) {
+        outfile << current->data.Name << ","
+            << current->data.type << "\n";
+        current = current->next;
+    }
+    outfile.close();
+}
+
+QLKH* importCoursesFromFile(const string& KhoaHocFile) {
+    ifstream infile(KhoaHocFile);
+    QLKH* newList = NULL;
+    if (!infile) {
+        cout << "ERROR: khong the nhap vao file!" << endl;
+        return NULL;
+    }
+    string line;
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string token;
+        vector<string> tokens;
+        while (getline(ss, token, ',')) {
+            tokens.push_back(token);
+        }
+        if (tokens.size() == 2) {
+            string Name = tokens[0];
+            string type = tokens[1];
+            addKhoaHoc(newList,Name, type);
+        }
+    }
+    infile.close();
+    return newList;
+}
---
+++ studentFile.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<vector>
+
+void exportStudentToFile(Student S, string& studentFile) {
+    ofstream outfile(studentFile);
+    if (!outfile) {
+        std::cout << "ERROR: khong the viet vao file!" << endl;
+        return;
+    }
+    NodeStudent* current = S;
+    while (current != nullptr) {
+        outfile << current->data.ID << ","
+            << current->data.Name << ","
+            << current->data.Age << ","
+            << current->data.Level << ","
+            << current->data.Gender << "\n";
+        current = current->next;
+    }
+    outfile.close();
+}
+
+NodeStudent* importStudentsFromFile(const string& studentFile) {
+    ifstream infile(studentFile);
+    NodeStudent* newList = NULL;
+    if (!infile) {
+        cout << "ERROR: khong the nhap vao file!" << endl;
+        return NULL;
+    }
+    string line;
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string token;
+        vector<string> tokens;
+        while (getline(ss, token, ',')) {
+            tokens.push_back(token);
+        }
+        if (tokens.size() == 5) {
+            string ID = tokens[0];
+            string Name = tokens[1];
+            int Age; stringstream ageStream(tokens[2]); ageStream >> Age;
+            string Level = tokens[3];
+            string Gender = tokens[4];
+            addStudent(newList, ID, Name, Age, Level, Gender);
+        }
+    }
+    infile.close();
+    return newList;
+}

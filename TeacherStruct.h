5e809cd225bd03f6588b030ac6cd5ea32a4cdb3f
lelinh09122005 <linhdepzaixyz09122005@gmail.com>
2025-06-05 21:50:48 +0700

    TeacherStruct.h

--- TeacherStruct.h
+++ TeacherStruct.h
@@ -1,11 +1,13 @@
 #pragma once
-#include "TeacherStruct.cpp"
-#include "ClassStruct.h"
+#include <string>
+#include "Class.h"
+
+struct QLKH;
 
 struct Teacher{
-    string ID;
-    string name;
-    string exp;  //trình độ
+    std::string ID;
+    std::string name;
+    std::string exp;  //trình độ
     int wexp;  //năm kinh nghiệm
 };
 
@@ -16,23 +18,20 @@ struct NodeTeacher{
 };
 
 void initTeacherDS(NodeTeacher* &gv);
-
 NodeTeacher* createNodeTeacher(Teacher data);
-
 void addTeacher(NodeTeacher* &gv, Teacher data);
-
-NodeTeacher* searchTeacher(NodeTeacher* &gv, string data);
-
-NodeTeacher* deleteTeacher(NodeTeacher* &gv, string data);
-
+NodeTeacher* searchTeacher(NodeTeacher* &gv, std::string data);
+NodeTeacher* deleteTeacher(NodeTeacher* &gv, std::string data);
 void displayTeacherInfo(NodeTeacher* gv);
-
 void displayTeacherList(NodeTeacher* gv);
-
 void addTeacherInfo(Teacher data);
-
 void changeTeacherInfoForCourse(NodeTeacher*& gv);
+void changeTeacherInfo(NodeTeacher*& gv);
+void TeacherManagement(QLKH *&course);
 
+inline void ensureInitTeacherList(NodeTeacher*& gv) {
+    if (gv == nullptr) gv = nullptr;
+}
 void changeTeacherInfo(NodeTeacher*& gv);
 
 void TeacherManagement(QLKH *&course);

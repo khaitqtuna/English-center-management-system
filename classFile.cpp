aa581dc0e381023bd1f0d6507b9445e120f1bbef
Vũ Nam Khánh <167059662+kwGuardian@users.noreply.github.com>
2025-06-05 14:58:58 +0700

    Add files via upload

---
+++ classFile.cpp
@@ -0,0 +1,48 @@
+﻿﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "ClassStruct.h"
+
+void exportClassToFile(NodeClass* C, string& ClassFile) {
+    ofstream outfile(ClassFile, ios::out | ios::trunc);
+    if (!outfile.is_open()) {
+        std::cout << "File dang duoc mo, khong the ghi vao file!" << endl;
+        return;
+    }
+    NodeClass* current = C;
+    while (current != nullptr) {
+        outfile << current->data.ID << ","
+            << current->data.name << ","
+            << current->data.studentCount << endl;
+        current = current->next;
+    }
+    outfile.close();
+}
+
+NodeClass* importClassFromFile(const string& ClassFile) {
+    ifstream infile(ClassFile);
+    if (!infile.is_open()) {
+        cout << "File dang duoc mo, khong the doc vao file!" << endl;
+        return NULL;
+    }
+    NodeClass* newList = NULL;
+    string line;
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string token;
+        vector<string> tokens;
+        while (getline(ss, token, ',')) {
+            tokens.push_back(token);
+        }
+        if (tokens.size() == 3) {
+            string ID = tokens[0];
+            string name = tokens[1];
+            int studentCount; stringstream studentCountStream(tokens[2]); studentCountStream >> studentCount;
+            addStudent(newList, ID, Name, studentCount);
+        }
+    }
+    infile.close();
+    return newList;
+}
---
+++ teacherFile.cpp
@@ -0,0 +1,50 @@
+﻿﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "TeacherStruct.h"
+
+void exportTeacherToFile(TeacherStudent* T, string& TeacherFile) {
+    ofstream outfile(TeacherFile, ios::out | ios::trunc);
+    if (!outfile.is_open()) {
+        std::cout << "File dang duoc mo, khong the ghi vao file!" << endl;
+        return;
+    }
+    NodeTeacher* current = T;
+    while (current != nullptr) {
+        outfile << current->data.ID << ","
+            << current->data.name << ","
+            << current->data.exp << ","
+            << current->data.wexp << endl;
+        current = current->next;
+    }
+    outfile.close();
+}
+
+NodeTeacher* importTeacherFromFile(const string& TeacherFile) {
+    ifstream infile(TeacherFile);
+    if (!infile.is_open()) {
+        cout << "File dang duoc mo, khong the doc vao file!" << endl;
+        return NULL;
+    }
+    NodeTeacher* newList = NULL;
+    string line;
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string token;
+        vector<string> tokens;
+        while (getline(ss, token, ',')) {
+            tokens.push_back(token);
+        }
+        if (tokens.size() == 4) {
+            string ID = tokens[0];
+            string name = tokens[1];
+            int exp; stringstream expStream(tokens[2]); expStream >> exp;            
+            int wexp; stringstream wexpStream(tokens[3]); wexpStream >> wexp;
+            addStudent(newList, ID, Name, Age, exp, wexp);
+        }
+    }
+    infile.close();
+    return newList;
+}
